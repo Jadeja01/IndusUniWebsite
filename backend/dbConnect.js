f3822f154e1c3f9eeac688b6858810e5b1a64553
Tirthraj <jadejatirthrajsinh01@gmail.com>
2026-05-14 19:37:03 +0530

    Fixing mongodb url

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -1,14 +1,24 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     const connectionString = process.env.MONGODB_URL;
+
+    if (!connectionString) {
+      throw new Error("MONGODB_URL not found");
+    }
+
     const conn = await mongoose.connect(connectionString);
+
+    console.log("MongoDB connected");
+
     return conn;
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error("MongoDB connection error:");
+    console.error(error);
+
+    throw error;
   }
-}
+};
 
 export default connectDB;
